632f329acf35bb353f52976b5f584daced8a1645
xala3pa <salazar3pa@gmail.com>
2014-11-10 23:39:46 +0100

    variables

---
+++ variables/variables.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string = "Álvaro"
+	fmt.Println(name)
+
+	var x, y int = 1, 2
+	fmt.Println(x, y)
+
+	var withoutType = true
+	fmt.Println(withoutType)
+
+	var notInit int
+	fmt.Println(notInit)
+
+	f := "shortHand"
+	fmt.Println(f)
+}
