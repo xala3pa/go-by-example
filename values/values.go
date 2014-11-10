b41e79b2e7cf878e8a2d553f304cda1a8982c40e
xala3pa <salazar3pa@gmail.com>
2014-11-10 23:33:08 +0100

    values

---
+++ values/values.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hola" + "Mundo")
+
+	fmt.Println("1+1 = ", 1+1)
+
+	fmt.Println("5.0/2.0 = ", 5.0/2.0)
+
+	fmt.Println(!true)
+	fmt.Println(true || false)
+	fmt.Println(false && true)
+}
