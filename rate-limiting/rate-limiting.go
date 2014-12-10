23e0854c1693dfeadedb828d3148822b2805d355
xala3pa <salazar3pa@gmail.com>
2014-12-10 23:23:29 +0100

    Rate Limiting

--- rate-limiting/rate-limiting.go
+++ rate-limiting/rate-limiting.go
@@ -23,4 +23,25 @@ func main() {
 		fmt.Println("request", req, time.Now())
 	}
 
+	burstyLimiter := make(chan time.Time, 3)
+
+	for i := 0; i < 3; i++ {
+		burstyLimiter <- time.Now()
+	}
+
+	go func() {
+		for t := range time.Tick(time.Millisecond * 200) {
+			burstyLimiter <- t
+		}
+	}()
+
+	burstyRequests := make(chan int, 5)
+	for i := 1; i <= 5; i++ {
+		burstyRequests <- i
+	}
+	close(burstyRequests)
+	for req := range burstyRequests {
+		<-burstyLimiter
+		fmt.Println("request", req, time.Now())
+	}
 }
